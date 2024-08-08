d87c9ab5ace78ffed9cca38bdefe61f166e424bd
Ajaykumar-N-GITHUB <ajayjothika17@gmail.com>
2024-08-09 00:31:02 +0530

    second

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Ajaykumar-N-GITHUB/marine
+
+go 1.22.0
---
+++ marine.go
@@ -0,0 +1,9 @@
+package marine
+
+func Seafisher(k string) string {
+	return k + " also, he knows marine fishing"
+}
+
+func Teaching(m string) string {
+	return m + " he is a Teacher "
+}
